1d980e82693ba7b4129611d0f30613e16eb622f1
konceptsandcode <koncepts.and.code@gmail.com>
2015-11-05 12:48:13 -0500

    adding badge text

--- HelloWorld/ui.js
+++ HelloWorld/ui.js
@@ -1,5 +1,9 @@
+var counter = 0;
+
 $(function() {
 	$('#clickDiv').click(function() {
-		alert("Hello World!");
+		alert("Hello World! from alert");
+		counter++;
+		chrome.browserAction.setBadgeText({"text": counter.toString()});
 	});
 }); 
